d8da42d035c8ccb94ffe3bc89c53d043e90fdc1e
Aala Altubi <aala.altubi@gmail.com>
2025-07-20 10:49:31 +0400

    Menu display added

--- ScientificCalculator/ScientificCalculator.java
+++ ScientificCalculator/ScientificCalculator.java
@@ -8,8 +8,29 @@ public class ScientificCalculator {
         Scanner inputReader = new Scanner(System.in);
 
     }
-    public static void displayMenu() {
 
-        
+    //==Menu display==
+    public static void displayMenu() {
+        System.out.println("Scientific Calculator");
+        System.out.println("1.  Addition");
+        System.out.println("2.  Subtraction");
+        System.out.println("3.  Multiplication");
+        System.out.println("4.  Division");
+        System.out.println("5.  Square Root");
+        System.out.println("6.  Power (x^y)");
+        System.out.println("7.  Sine");
+        System.out.println("8.  Cosine");
+        System.out.println("9.  Tangent");
+        System.out.println("10. Natural Logarithm (ln x)");
+        System.out.println("11. Logarithm Base 10");
+        System.out.println("12. Absolute Value");
+        System.out.println("13. Round to Nearest Long");
+        System.out.println("14. Ceiling");
+        System.out.println("15. Floor");
+        System.out.println("16. Minimum of Two");
+        System.out.println("17. Maximum of Two");
+        System.out.println("0.  Exit");
+
     }
+    
 }
