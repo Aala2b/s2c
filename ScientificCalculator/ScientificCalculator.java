ee688c71b14296f05cd6c611e97c2f4a111695c6
Aala Altubi <aala.altubi@gmail.com>
2025-07-20 10:31:57 +0400

    feat: Initial project setup for ScientificCalculator

---
+++ ScientificCalculator/ScientificCalculator.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+import java.util.InputMismatchException; // Needed for handling non-numeric input
+
+public class ScientificCalculator {
+    double pi= 3.1415;
+
+    public static void main(String[] args){
+        Scanner inputReader = new Scanner(System.in);
+
+    }
+    public static void displayMenu() {
+
+        
+    }
+}
